188ac9a8801c360bf877ed41e365f00507e0950c
wail-uottawa <wgueaieb@uottawa.ca>
2024-10-12 19:29:49 -0400

    Updated path in configurations file

--- configurations.bash
+++ configurations.bash
@@ -25,3 +25,25 @@
 # https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Colcon-Tutorial.html
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=/opt/ros/humble/
+
+### Aliases
+alias ros2pkgcreate="ros2 pkg create --build-type ament_python --license Apache-2.0"
+
+# enable color support of ls and also add handy aliases
+# https://askubuntu.com/questions/17299/what-do-the-different-colors-mean-in-ls
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+
+### Update path
+if [ -d "$HOME/.local/bin" ] ; then
+  PATH="$PATH:$HOME/.local/bin"
+fi
