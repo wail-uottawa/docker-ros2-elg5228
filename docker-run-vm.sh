5bbfd37f0d42d195afb8e89a7e648e2fa19ac987
wail-uottawa <wgueaieb@uottawa.ca>
2025-06-26 15:30:11 -0400

    Updated for Fall 2025

--- docker-run-vm.sh
+++ docker-run-vm.sh
@@ -5,7 +5,7 @@ docker run -it --rm --privileged --name docker-ros2-humble-elg5228 \
        --security-opt seccomp=unconfined \
        --shm-size=512m \
        --volume ~/course_dir:/home/ubuntu/ros2_ws/src/course_dir \
-       realjsk/docker-ros2-humble-elg5228:20240503 \
+       realjsk/docker-ros2-humble-elg5228:20250616 \
        bash
 
 # Browse http://127.0.0.1:6080/
